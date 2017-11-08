dc894124f47e6f6cad4d65289cf2c0d14d181f92
EUROPE\WEBSTEM1 <webstem1@airproducts.com>
2017-11-08 15:50:40 +0000

    Initial commit

---
+++ SysSpeech/SysSpeech/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Speech.Recognition;
+
+namespace SysSpeech
+{
+    public class Program
+    {
+        public static SpeechRecognitionEngine recEngine = new SpeechRecognitionEngine();
+
+        public static void Main(string[] args)
+        {
+            Choices commands = new Choices();
+            commands.Add(new string[] { "say hello", "print my name" });
+
+            GrammarBuilder gBuilder = new GrammarBuilder();
+            gBuilder.Append(commands);
+
+            Grammar grammar = new Grammar(gBuilder);
+
+            recEngine.LoadGrammar(grammar);
+            recEngine.SetInputToDefaultAudioDevice();
+            recEngine.SpeechRecognized += recEngine_SpeechRecognized;
+
+            recEngine.RecognizeAsync(RecognizeMode.Multiple);
+
+            recEngine.RecognizeAsyncStop();
+
+            Console.ReadLine();
+        }
+
+        public static void recEngine_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            Console.WriteLine(e.Result.Text);
+        }
+    }
+}
